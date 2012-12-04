c18db30b0c621ee4475d3bc4e27ae70064e9cd42
Kirill Bezrukov <kirill.bezrukov@gmail.com>
2012-12-04 14:03:39 +0000

    page
    git-svn-id: svn://redminecrm.com/redmine_cms/trunk@2274 eca72092-3dbd-4bd6-9333-8708c660e71c

--- app/controllers/project_tabs_controller.rb
+++ app/controllers/project_tabs_controller.rb
@@ -7,7 +7,7 @@ class ProjectTabsController < ApplicationController
 
   def show
     menu_items[:project_tabs][:actions][:show] = "project_tab_#{params[:tab]}".to_sym
-    @page = Page.find_by_name(ContactsSetting["project_tab_#{params[:tab]}_wiki_page", @project.id])
+    @page = Page.find_by_name(ContactsSetting["project_tab_#{params[:tab]}_page", @project.id])
     render_404 unless @page    
   end
 
