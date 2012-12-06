92c726077b56200aa8b014342756dd7651b73cd1
Kirill Bezrukov <kirill.bezrukov@gmail.com>
2012-12-06 21:54:43 +0000

    Type commit message here…
    git-svn-id: svn://redminecrm.com/redmine_cms/trunk@2292 eca72092-3dbd-4bd6-9333-8708c660e71c

--- db/migrate/003_create_parts.rb
+++ db/migrate/003_create_parts.rb
@@ -5,7 +5,6 @@ class CreateParts < ActiveRecord::Migration
       t.string :part_type
       t.text :content
       t.string :content_type
-      t.integer :position
       t.boolean :is_cached, :default => false
       t.timestamps 
     end
@@ -13,6 +12,7 @@ class CreateParts < ActiveRecord::Migration
     create_table :pages_parts do |t|
       t.integer :page_id
       t.integer :part_id
+      t.integer :position
       t.integer :status_id, :default => RedmineCms::STATUS_LOCKED
     end
     add_index :pages_parts, [:page_id, :part_id]
