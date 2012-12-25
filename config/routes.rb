2c3337072439a42ee258286cea6b3486af6db9e7
Kirill Bezrukov <kirill.bezrukov@gmail.com>
2012-12-25 13:05:50 +0000

    CodeMirror
    git-svn-id: svn://redminecrm.com/redmine_cms/trunk@2399 eca72092-3dbd-4bd6-9333-8708c660e71c

--- config/routes.rb
+++ config/routes.rb
@@ -18,3 +18,4 @@ match "pages/:page_id/add_part" => "pages_parts#add", :via => :post
 match "pages/:page_id/delete_part/:part_id" => "pages_parts#delete", :via => :delete
 match "pages/:page_id/update/:part_id" => "pages_parts#update", :via => :put
 
+match 'attachments/thumbnail/:id(/:size)/:filename', :controller => 'attachments', :action => 'thumbnail', :id => /\d+/, :filename => /.*/, :via => :get, :size => /\d+/
