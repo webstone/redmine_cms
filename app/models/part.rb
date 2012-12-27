1843834ec71e58e3e4215a57b2677bef74ceea00
Kirill Bezrukov <kirill.bezrukov@gmail.com>
2012-12-27 21:41:47 +0000

    Type commit message here…
    git-svn-id: svn://redminecrm.com/redmine_cms/trunk@2428 eca72092-3dbd-4bd6-9333-8708c660e71c

--- app/models/part.rb
+++ app/models/part.rb
@@ -16,6 +16,16 @@ class Part < ActiveRecord::Base
   validates_presence_of :name, :part_type, :content_type
   validates_format_of :name, :with => /^(?!\d+$)[a-z0-9\-_]*$/
 
+ [:content, :header, :footer, :sidebar].each do |name, params|
+    src = <<-END_SRC
+    def is_#{name}_type?
+      self.part_type.strip.downcase == "#{name.to_s}"
+    end
+    END_SRC
+    class_eval src, __FILE__, __LINE__
+  end
+
+
 private
   def touch_pages
     pages.each{|p| p.touch} if pages
