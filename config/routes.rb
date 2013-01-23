c10e50714b354d6c90c1f8a4314755f49b95a359
Kirill Bezrukov <kirill.bezrukov@gmail.com>
2013-01-23 15:24:13 +0000

    sitemap
    git-svn-id: svn://redminecrm.com/redmine_cms/trunk@2610 eca72092-3dbd-4bd6-9333-8708c660e71c

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 
 match "/sitemap.xml" => "welcome#sitemap", :defaults => {:format => :xml}
 
-match "projects/:project_id/pages/:tab" => "project_tabs#show"
+match "projects/:project_id/pages/:tab" => "project_tabs#show", :as => "project_tab"
 match "products/:project_id" => "project_tabs#show"
 
 resources :cms_menus
