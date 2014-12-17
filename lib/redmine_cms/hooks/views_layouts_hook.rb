fbc32897d5ff74a72c08f184a9a5111badc68d7a
Kirill Bezrukov <kirill.bezrukov@gmail.com>
2014-12-17 13:16:14 +0300

    jqyery migrate

--- lib/redmine_cms/hooks/views_layouts_hook.rb
+++ lib/redmine_cms/hooks/views_layouts_hook.rb
@@ -2,7 +2,7 @@ module RedmineCms
   module Hooks
     class ViewsLayoutsHook < Redmine::Hook::ViewListener
       def view_layouts_base_html_head(context={})
-        return stylesheet_link_tag(:cms, :plugin => 'redmine_cms') 
+        return stylesheet_link_tag(:cms, :plugin => 'redmine_cms') + javascript_include_tag("jquery-migrate-1.2.1.js", :plugin => 'redmine_cms')
       end
       def view_layouts_base_body_bottom(context = { })
         return Setting.plugin_redmine_cms[:layout_content].html_safe if Setting.plugin_redmine_cms[:layout_content]
