9476234bea346aa507add01b278e256864c19934
kunashir <kunashir@list.ru>
2015-07-31 18:40:41 +0300

    start work with test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,26 @@
 # Load the Redmine helper
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
+
+class RedmineCMS::TestCase
+  include ActionDispatch::TestProcess
+  def self.plugin_fixtures(plugin, *fixture_names)
+    plugin_fixture_path = "#{Redmine::Plugin.find(plugin).directory}/test/fixtures"
+    if fixture_names.first == :all
+      fixture_names = Dir["#{plugin_fixture_path}/**/*.{yml}"]
+      fixture_names.map! { |f| f[(plugin_fixture_path.size + 1)..-5] }
+    else
+      fixture_names = fixture_names.flatten.map { |n| n.to_s }
+    end
+
+    ActiveRecord::Fixtures.create_fixtures(plugin_fixture_path, fixture_names)
+  end
+
+  def self.create_fixtures(table_names, class_names = {})
+    fixtures_directory = Redmine::Plugin.find(:redmine_cms).directory + '/test/fixtures/'
+    if ActiveRecord::VERSION::MAJOR >= 4
+      ActiveRecord::FixtureSet.create_fixtures(fixtures_directory, table_names, class_names = {})
+    else
+      ActiveRecord::Fixtures.create_fixtures(fixtures_directory, table_names, class_names = {})
+    end
+  end
+end
--- test/unit/page_test.rb
+++ test/unit/page_test.rb
@@ -2,8 +2,49 @@ require File.expand_path('../../test_helper', __FILE__)
 
 class PageTest < ActiveSupport::TestCase
 
+  fixtures :users, :pages
+
+  RedmineCMS::TestCase.create_fixtures(:pages)
   # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_create_page
+    page = Page.new
+    assert !page.save, 'Can save a page without name and title'
+    page.name = 'first_page'
+    assert !page.save, 'Can save a page without title'
+    page.name = nil
+    page.title = 'first page'
+    assert !page.save, 'Can save a page without name'
+    page.name = "first_page"
+    assert page.save, 'Cannot save a page with name and title'
+  end
+
+  def test_validation_for_name
+    page = Page.new(:title => "Page")
+    page.name = "invalid name"
+    assert !page.save, "Can save a page with invalid name include space" 
+    page.name = "invalid&name"
+    assert !page.save, "Can save a page with invalid name included &"
+    page.name = "invalid?name"
+    assert !page.save, "Can save a page with invalid name included ?"
+    page.name = "valid_name"
+    assert page.save, 'Cannot save a page with valid name'
+  end
+
+  def test_uniq_name
+    page = Page.new(:title => "Page", :name => "page_001")
+    assert !page.save, "Can save page with not uniq name"
+  end
+
+  def test_visible
+    user = users(:users_001) #admin
+    page = pages(:page_001)
+    assert page.visible?(user), 'Page does not visible for admin user'
+    # not active page
+
+    # anonymus user
+
+    # logged page
+
+    # public page
   end
 end
